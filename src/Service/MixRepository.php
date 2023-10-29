f5d83659fbc743a142497caeb1c5f50c24f01a20
Christopher Ciampoli <chrisciampoli@gmail.com>
2023-10-29 12:54:38 -0400

    Thats about all for this

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -8,13 +8,17 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MixRepository
 {
-    public function __construct(private HttpClientInterface $client, private CacheInterface $cache){}
+    public function __construct(
+        private HttpClientInterface $githubContentClient,
+        private CacheInterface $cache,
+        bool $isDebug,
+    ){}
 
     public function findAll(): array
     {
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter(5);
-            $response = $this->client->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
         });
